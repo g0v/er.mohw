1bd53c4f908cd65f9b942455e6c70fd76ab48ab1
Chia-liang Kao <clkao@clkao.org>
2014-08-12 00:51:22 +0800

    regen for f367976

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,4 +1,4 @@
-/*! grafana - v1.7.0-rc1 - 2014-08-10
+/*! grafana - v1.7.0 - 2014-08-12
  * Copyright (c) 2014 Torkel Ödegaard; Licensed Apache License */
 
 module.exports=function(a){a.set({basePath:"../../",frameworks:["mocha","requirejs","expect","sinon"],files:["src/test/test-main.js",{pattern:"src/app/**/*.js",included:!1},{pattern:"src/vendor/**/*.js",included:!1},{pattern:"src/test/**/*.js",included:!1},{pattern:"src/**/*.js",included:!1}],exclude:[],reporters:["dots"],port:9876,colors:!0,logLevel:a.LOG_INFO,autoWatch:!0,browsers:["PhantomJS"],captureTimeout:6e4,singleRun:!0})};
--- vendor/require/tmpl.js
+++ vendor/require/tmpl.js
@@ -1,4 +1,4 @@
-/*! grafana - v1.7.0-rc1 - 2014-08-10
+/*! grafana - v1.7.0 - 2014-08-12
  * Copyright (c) 2014 Torkel Ödegaard; Licensed Apache License */
 
 define(["module"],function(a){var b=a.config&&a.config()||{};return{load:function(a,c,d){var e=c.toUrl(a);c(["text!"+a],function(a){b.registerTemplate&&b.registerTemplate(e,a),d(a)})}}});
