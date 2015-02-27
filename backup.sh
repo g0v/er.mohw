4b6c62eb94660acc058e93176b0c8155238deb8a
lanfon72 <lanfon72@gmail.com>
2015-02-28 03:44:59 +0800

    fix lost data, and add output error.log

--- backup.sh
+++ backup.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
 now=$(date "+%Y-%m-%d_%H-%M")
-grep -e "{" temp | grep -e "}" |grep -v "null" | sed 's/.*\({.*}\).*/\1/g' >> "$now"
+sed 's/\(}{\)/}\'$'\n{/g' temp |\
+sed 's/.*\({.*}\).*/\1/g' |\
+grep -e "{\(.*\:.*\)}" >>"$now"
+#grep -e "{" temp | grep -e "}" |grep -v "null" | sed 's/.*\({.*}\).*/\1/g' >> "$now"
+
+sed "1,35d" temp | \
+sed "s/allresolved/\\n`wc -l $now`/g" | \
+sed 's/\(}{\)/}\'$'\n{/g' | \
+sed "/.*:.*[,|}]/d" >> error.log
+
 mv "$now" backup/
